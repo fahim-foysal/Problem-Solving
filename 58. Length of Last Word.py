cdae6b180c55662d5ab8ee8f2e186bac3101796e
Fahim Foysal <fahimfoysalcse@gmail.com>
2023-09-02 22:33:29 +0600

    More efficient solution

--- 58. Length of Last Word.py
+++ 58. Length of Last Word.py
@@ -4,20 +4,21 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        length = len(s)
-        count = 0
-        flag = 0
-        while length-1 >= 0:
-            if s[length-1] != ' ':   
-                count += 1
-                flag = 1
-            if s[length-1] == ' ' and flag == 1:
-                break
-            length -= 1
-        print(count)
+        # length = len(s)
+        # count = 0
+        # flag = 0
+        # while length-1 >= 0:
+        #     if s[length-1] != ' ':   
+        #         count += 1
+        #         flag = 1
+        #     if s[length-1] == ' ' and flag == 1:
+        #         break
+        #     length -= 1
+        
         
         """ first solution with bit less efficiency"""
-        # word_list = s.split()
-        # return len(word_list[(len(word_list)-1)])
+        word_list = s.split()
+        print(len(word_list[-1]))
+        return len(word_list[-1])
 s = Solution()
-s.lengthOfLastWord("a")
+s.lengthOfLastWord("adfg fkja  ")
